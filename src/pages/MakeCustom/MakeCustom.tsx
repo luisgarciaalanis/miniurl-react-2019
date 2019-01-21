6df13480eec2dec689683fff1ee73bf9b3f45a51
Luis Garcia Alanis <luis@garcia.tv>
2019-01-20 22:19:34 -0800

    pages mini contents

--- src/pages/MakeCustom/MakeCustom.tsx
+++ src/pages/MakeCustom/MakeCustom.tsx
@@ -5,6 +5,15 @@ class MakeCustom extends React.Component {
         return (
             <div>
                 Make custom!
+                <label htmlFor="urlinput">Enter a URL</label>
+                <input type="text" name="urlinput" id="urlinput" />
+                <label htmlFor="hash">Enter alias</label>
+                <input type="text" name="hash" id="hash" />
+                <input type="button" value="Make tiny" />
+                <div>
+                    <span>MiniURL was created:</span>
+                    <span><a href="http://www.google.com/">http://www.google.com/</a></span>
+                </div>
             </div>
         );
     }
--- src/pages/MakeTiny/MakeTiny.tsx
+++ src/pages/MakeTiny/MakeTiny.tsx
@@ -5,6 +5,13 @@ class MakeTiny extends React.Component {
         return (
             <div>
                 Make tiny!
+                <label htmlFor="urlinput">Enter a URL</label>
+                <input type="text" name="urlinput" id="urlinput" />
+                <input type="button" value="Make tiny" />
+                <div>
+                    <span>MiniURL was created:</span>
+                    <span><a href="http://www.google.com/">http://www.google.com/</a></span>
+                </div>
             </div>
         );
     }
